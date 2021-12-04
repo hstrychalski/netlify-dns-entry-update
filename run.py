df1bb3612ca3bf3af5eec66fab3839f70352a667
hstrychalski <hubert.strychalski@centra.com>
2021-12-04 13:11:20 +0100

    set log lvl error

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from dns_records_update_runner import DnsRecordsUpdateRunner
 dirname = os.path.dirname(__file__)
 log_filename = os.path.join(dirname, 'log/app.log')
 
-logging.basicConfig(filename=log_filename, level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logging.basicConfig(filename=log_filename, level=logging.ERROR, format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger = logging.getLogger(__name__)
 
 task_runner = DnsRecordsUpdateRunner()
