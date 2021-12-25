bec619567026f44cdccd0c7fc78e9043ecc68d3d
hstrychalski <hubert.strychalski@centra.com>
2021-12-25 19:03:57 +0100

    fix typo in logs dir path

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import logging
 from dns_records_update_runner import DnsRecordsUpdateRunner
 from api.helper import get_absolute_path
 
-log_filename = get_absolute_path('logs/app.log')
+log_filename = get_absolute_path('log/app.log')
 
 logging.basicConfig(filename=log_filename, level=logging.ERROR, format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger = logging.getLogger(__name__)
